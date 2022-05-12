aa37b3aa6a51b4fa061f8a0c8655e906ed9be639
Alexander <alexanderl96@yahoo.com>
2022-05-11 21:23:10 -0400

    TeamsPlayers component its set up

--- src/Components/teamsPlayers/TemasPlayers.js
+++ src/Components/teamsPlayers/TemasPlayers.js
@@ -4,6 +4,10 @@ import './TeamsPlayers.css'
 
 const TemasPlayers = () => {
   const [gruopPlayers, setGruopPlayers] = useState([]);
+  const [group, setGroup] = useState([]);
+  const [on , setON] = useState([true]);
+  const [on1 , setON1] = useState([true]);
+  const [more , setMore] = useState([]);
 
   // let  params = useParams();
   // let teamId = params.id
@@ -16,7 +20,15 @@ const TemasPlayers = () => {
   //     setGruopPlayers(data.individual.group)
   //   })
   // },[teamId])
-
+  // useEffect(()=>{
+  //   fetch(`http://localhost:9000/groups`)
+  //   .then(res => res.json())
+  //   .then(data =>{
+  //     console.log(data, 'temasPlayers');
+  //     setGruopPlayers(data)
+  //   })
+  // },[])
+ 
   useEffect(()=>{
     fetch(`http://localhost:9000/groups`)
     .then(res => res.json())
@@ -25,7 +37,20 @@ const TemasPlayers = () => {
       setGruopPlayers(data)
     })
   },[])
+console.log(group , 'group members')
 
+  const handleSub = (e) => {
+    e.preventDefault();
+    if(on ) {
+      setGroup(gruopPlayers);
+      setMore('');
+      setON(false)
+    }else{
+      setGroup('')
+      setMore("more...")
+      setON(true)
+    }
+  };
 
 
 
@@ -36,7 +61,8 @@ const TemasPlayers = () => {
       <div>
        {gruopPlayers.map((player, index) =>{
          return(
-           <div className="teamsPlayers">
+          <div>
+           <div className="teamsPlayers" value={on}  onClick={ handleSub}  >
              <div>
                 <img src={player?.imag} alt={player?.id} className="players_Img"/> 
                 <div key={index} className="players_Name">{player?.name}</div>
@@ -46,14 +72,53 @@ const TemasPlayers = () => {
                   <div className= 'playerDataInfo'><span>Number:</span> {player?.number}</div>
                   <div className= 'playerDataInfo'><span>Position:</span> {player?.position}</div>
                   <div className= 'playerDataInfo'><span>Salary: </span>{player?.salary}</div>
-                  <p className= 'playerDataInfo'>more...</p>
+                  <p className= 'playerDataInfo'   >{more}</p>
                 </div>
              </div>
            </div>
+           <div>
+           {group === '' ? null : group.map((player)=>{
+             return (
+               <div className="teamPlayer_Group">
+                 <div className="teamPlayer_About">
+                    <div><span>About:</span> {player.about}</div> 
+                 </div>
+                  
+                  <div className="teamPlayer_About">
+                    <div> <span>Age:</span> {player.age}</div> 
+                    <div> <span>Born:</span> {player.born}</div> 
+                    <div> <span>City: </span>{player.city}</div> 
+                    <div> <span>State:</span> {player.state}</div>
+                    <div> <span>Country:</span> {player.country}</div> 
+                    <div> <span>Education:</span> {player.education}</div>
+                  </div>
+                  <div className="teamPlayer_About">
+                    <div> <span>Parents:</span> {player.parents}</div> 
+                    <div> <span>Spouse: </span>{player.spouse}</div> 
+                    <div> <span>Children:</span> {player.children}</div>
+                    <div> <span>Siblings:</span> {player.siblings}</div>
+                  </div>
+                  <div className="teamPlayer_About">
+                    <div> <span>Nickname:</span> {player.nickname}</div>
+                    <div> <span>Bats:</span> {player.bats}</div>
+                    <div> <span>Throws:</span> {player.throws}</div>
+                    <div> <span>Height:</span> {player.height}</div>
+                    <div> <span>Weight: </span>{player.weight}</div>
+                  </div>
+                  <div className="teamPlayer_About">
+                    <div> <span>Stats: </span>{player.stats}</div>
+                  </div>
+               </div>
+             )
+           }) }
+           </div>
+           </div>
+
            
          )
        })}
       </div>
+      
      
     </div>
   )
