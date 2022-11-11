f7b18aa25a45b3c081de9fef5eab0cbdf3f6225a
Alexander <alexanderl96@yahoo.com>
2022-11-11 13:36:32 -0500

    setting media for about component

--- src/Components/AboutUs/about.js
+++ src/Components/AboutUs/about.js
@@ -4,6 +4,7 @@ import Footer from '../Footer/Footer'
 import Resume from '../PDF/Resume2022.pdf'
 import Logo from '../../image/FullLogo.png'
 import emailjs from '@emailjs/browser';
+import './about.css'
 import { FaGithub, FaLinkedin, FaTwitter, FaInstagram, FaYoutube, FaMobileAlt, FaMapMarkerAlt} from 'react-icons/fa'
 
 const About = () => {
@@ -20,15 +21,16 @@ const About = () => {
     let {value} = e.target
      nameUser = value
    }
-  //  const phoneTarget =(e)=>{
-  //    let {value} = e.target
-  //   //  if(String(value).split('').length !== 10) return 'Invalid number'
-  //    let len = String(value).split('').length
-  //    let first = String(value).split('').slice(0,3).join('')
-  //    let middle = String(value).split('').slice(3,6).join('')
-  //    let rest = String(value).split('').slice(6,len).join('')
-  //      `(${first}) ${middle}-${rest}`
-  //  }
+
+   const phoneTarget =(e)=>{
+      let {value} = e.target
+      let len = value.split('').length
+      let first = value.split('').slice(0,3).join('')
+     let middle = value.split('').slice(3,6).join('')
+      let rest = value.split('').slice(6,len).join('')
+      // return `(${first}) ${middle ? middle +'-': ''}${rest}`
+      console.log(`(${first}) ${middle ? middle +'-': ''}${rest}`)
+   }
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -57,82 +59,82 @@ const About = () => {
     <div>
         <NavBar />
         <div>
-            <div style={{display: 'flex', margin: '10px', justifyContent: 'flex-end'}}> 
+            <div className='media'> 
                   <a href='https://github.com/alexanderl96-hub/new_frontend'>
-                      <FaGithub    style={{fontSize: '25px', margin: '3px' }} /> 
+                      <FaGithub  className='mediaIcon' /> 
                   </a>
                   <a href='https://www.linkedin.com/in/alexander-la-rosa-p%C3%A9rez-2b36a085/'>
-                      <FaLinkedin   style={{fontSize: '25px', margin: '3px'}} /> 
+                      <FaLinkedin   className='mediaIcon' /> 
                   </a>
                   <a href='https://twitter.com/LperezAlexander' >
-                      <FaTwitter   style={{fontSize: '25px', margin: '3px'}} /> 
+                      <FaTwitter  className='mediaIcon' /> 
                   </a>
                   <a href='https://www.instagram.com/larosa_cubart/'>
-                      <FaInstagram  style={{fontSize: '25px', margin: '3px'}} /> 
+                      <FaInstagram  className='mediaIcon' /> 
                   </a>
                   <a href='https://youtu.be/rY_fkUZL2qI'>
-                      <FaYoutube   style={{fontSize: '25px', margin: '3px' }} /> 
+                      <FaYoutube  className='mediaIcon' /> 
                   </a>
             </div>
          
-            <div style={{display: 'flex', justifyContent: 'space-around', margin: '15px'}}>
+            <div className='Image-Description'>
 
                 <div >
                     <img src='https://raw.githubusercontent.com/alexanderl96-hub/my-website-dev/main/src/Images/Alex.jpg' alt='' 
-                    style={{height: '260px', borderRadius: '10px'}}/>
+                    className='aboutImage' />
                     <h4>Alexander La Rosa Pérez</h4>
                 </div>
           
-                <div style={{ width: '500px'}}>
-                  <p style={{ textAlign: 'justify',pointerEvents: 'auto'}}>From dancing and creating art, to building applications and choreographing code; 
+                <div className='description'>
+                  <p >From dancing and creating art, to building applications and choreographing code; 
                 I am a dancer turned software developer aspiring to have a positive impact on the world using 
                 technology.</p>
-                  <p style={{ textAlign: 'justify',pointerEvents: 'auto'}}>I recently joined the <a href="http://www.pursuit.org/" style={{textDecoration:'underline'}}>Pursuit</a> core program, which is an intensive <em>12 - month software engineering 
+                  <p >I recently joined the <a href="http://www.pursuit.org/" >Pursuit</a> core program, which is an intensive <em>12 - month software engineering 
                     fellowship</em>, with a 9 percent acceptance rate. As a creative and 
                     diligent tech nerd I am a firm believer in the positive power of technology. My goal is 
                     to build and create programs that bring people closer together.  As a former performing 
-                    artist, I am passionate about the <a href="https://www.instagram.com/p/B3TDzcgnO4M/" style={{textDecoration:'underline'}}>arts</a>, music and <a href="https://youtu.be/rY_fkUZL2qI" style={{textDecoration:'underline'}}>dance</a>. The 
+                    artist, I am passionate about the <a href="https://www.instagram.com/p/B3TDzcgnO4M/" >arts</a>, music and <a href="https://youtu.be/rY_fkUZL2qI" >dance</a>. The 
                     Pursuit program has given me the chance to develop another creative outlet, and even 
                     though I spend most of my time in front of the computer these days I still perform, play 
                     drums and sing in my spare time.</p>
-                  <p style={{ textAlign: 'justify',pointerEvents: 'auto'}}>My journey with Pursuit will end in a year from now, after which I hope to achieve 
+                  <p >My journey with Pursuit will end in a year from now, after which I hope to achieve 
                     my goals and to be able to give more to my community – not just as a person but also as a
                     professional.</p>
                 </div>
             </div>
 
-            <div style={{background: '#070f3c', height: '95px', color: 'white', textAlign: 'center', paddingTop: '8px'}}>
+            <div className='technology'>
                Software Developer / Technology   
-                 <p style={{color:'red', fontSize: '14px', display:'flex', justifyContent: 'space-around', height: '20px', pointerEvents: 'auto'}}>
-                   <a href='https://www.codecademy.com/learn/introduction-to-javascript' style={{color:'red'}}>• JavaScript •</a>
-                   <a href='https://react-redux.js.org/tutorials/quick-start' style={{color:'red'}} >• React/Redux •</a>
-                   <a href='https://nodejs.org/en/' style={{color:'red'}} >• Node.js •</a>
-                   <a href='https://www.w3schools.com/html/html_intro.asp' style={{color:'red'}} >• HTMl •</a>
-                   <a href='https://www.w3schools.com/css/' style={{color:'red'}} >• CSS •</a>
-                   <a href='https://expressjs.com/' style={{color:'red'}} >• Express •</a>
-                   <a href='https://product.hubspot.com/blog/git-and-github-tutorial-for-beginners' style={{color:'red'}} >• Git/GitHub •</a>
-                   <a href='https://www.postgresql.org/' style={{color:'red'}} >• PostgreSQL •</a>
-                   <a href='https://rapidapi.com/category/Database' style={{color:'red'}} >• APIs •</a>
-                   <a href='https://code.visualstudio.com/' style={{color:'red'}} >• VS Code •</a>
-                   <a href='https://www.geeksforgeeks.org/data-structures/' style={{color:'red'}} >• Data Structures •</a>
-                   <a href='https://www.geeksforgeeks.org/introduction-to-algorithms/' style={{color:'red'}} >• Algorithms •</a>
+                 <p >
+                   <a href='https://www.codecademy.com/learn/introduction-to-javascript' >• JavaScript •</a>
+                   <a href='https://react-redux.js.org/tutorials/quick-start'  >• React/Redux •</a>
+                   <a href='https://nodejs.org/en/'  >• Node.js •</a>
+                   <a href='https://www.w3schools.com/html/html_intro.asp' >• HTMl •</a>
+                   <a href='https://www.w3schools.com/css/'  >• CSS •</a>
+                   <a href='https://expressjs.com/' >• Express •</a>
+                   <a href='https://product.hubspot.com/blog/git-and-github-tutorial-for-beginners' >• Git/GitHub •</a>
+                   <a href='https://www.postgresql.org/' >• PostgreSQL •</a>
+                   <a href='https://rapidapi.com/category/Database'  >• APIs •</a>
+                   <a href='https://code.visualstudio.com/'  >• VS Code •</a>
+                   <a href='https://www.geeksforgeeks.org/data-structures/'  >• Data Structures •</a>
+                   <a href='https://www.geeksforgeeks.org/introduction-to-algorithms/'  >• Algorithms •</a>
                    </p>
-                 <a href={Resume} style={{color: 'white', textDecoration: 'underline', fontSize: '14px',}}>Resume</a>
+                 <a href={Resume} className='resume'>Resume</a>
             </div>
         
-            <div style={{display:'flex', justifyContent: 'space-around', margin: '20px'}}>
-                  <div style={{height: '80px', width: '250px', border: '2px solid #070f3c', padding: '10px', textAlign: 'center'}}>
-                         <img src={Logo} alt='' style={{height: '40px', borderRadius: '50px'}}/>
+            <div  className='concatInfo'>
+                  <div className='concatInfoWeb'>
+                         <img src={Logo} alt='' />
                          <p><span>Contact:</span> Sport World</p>
                   </div>
                 
-                  <div style={{height: '80px', width: '250px', border: '2px solid #070f3c', padding: '10px', textAlign: 'center'}}>
-                         <a href='https://www.google.com/maps/place/626+Riverside+Dr,+New+York,+NY+10031/@40.8237005,-73.9568611,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2f666973658dd:0x10fef5e48a024bc1!8m2!3d40.8236965!4d-73.9546724'> <FaMapMarkerAlt style={{fontSize: '28px'}}/> </a>
+                  <div className='concatInfoWeb'>
+                         <a href='https://www.google.com/maps/place/626+Riverside+Dr,+New+York,+NY+10031/@40.8237005,-73.9568611,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2f666973658dd:0x10fef5e48a024bc1!8m2!3d40.8236965!4d-73.9546724'> <FaMapMarkerAlt  className='icon' /> </a>
                          <p><span>Location:</span> 626 Riverside Drive, New York, NY, 10031</p>      
                   </div>
                
-                  <div style={{height: '80px', width: '250px', border: '2px solid #070f3c', padding: '10px', textAlign: 'center'}}>
-                          <FaMobileAlt style={{fontSize: '28px'}}/>
+                  <div  className='concatInfoWeb'>
+                          <FaMobileAlt  className='icon' />
                           <p><span>Phone:</span> (347) 697-3775</p>   
                   </div>
                 
@@ -151,7 +153,7 @@ const About = () => {
                     <label htmlFor="for" />
                     <input type='email' name="from_email" placeholder='Email...' required  style={{ borderStyle: 'dashed',width: '300px', height: '30px' }} />
                     <label htmlFor="for" />
-                    <input type='text' name="from_phone"  placeholder='Phone...' required  style={{ borderStyle: 'dashed', width: '300px', height: '30px' }}  />
+                    <input type='text' name="from_phone" onChange={phoneTarget}  placeholder='Phone...' required  style={{ borderStyle: 'dashed', width: '300px', height: '30px' }}  />
                     <label htmlFor="for" />
                     <textarea name='message' placeholder='Message...' required  style={{ borderStyle: 'dashed',width: '300px', height: '200px'}} />
                     <button type='submit' value="Send" style={{ margin:'5px', width: '70px', borderRadius:'10px', cursor: 'pointer', }}  >Submit</button>
