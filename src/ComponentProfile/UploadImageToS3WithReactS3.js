0a8f312fa242e8196f47721704fa5e1b02d1f9ab
Alexander <alexanderl96@yahoo.com>
2022-12-05 23:59:42 -0500

    git check

--- src/ComponentProfile/UploadImageToS3WithReactS3.js
+++ src/ComponentProfile/UploadImageToS3WithReactS3.js
@@ -1,12 +1,13 @@
 import React , {useState} from 'react';
 import { uploadFile } from 'react-s3';
+import {S3_BUCKET, REGION, ACCESS_KEY, SECRET_ACCESS_KEY } from '../../.env'
 
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
-const S3_BUCKET ='baseballsportworld';
-const REGION ='us-east-1';
-const ACCESS_KEY ='AKIA2H7E3HU6YWUH4BO6';
-const SECRET_ACCESS_KEY ='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';
+// const S3_BUCKET ='baseballsportworld';
+// const REGION ='us-east-1';
+// const ACCESS_KEY ='AKIA2H7E3HU6YWUH4BO6';
+// const SECRET_ACCESS_KEY ='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';
 
 const config = {
     bucketName: S3_BUCKET,
